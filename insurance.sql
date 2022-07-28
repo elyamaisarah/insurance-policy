938e33fa78e8009dcf3944c3ae1b34e84f83ae3f
elyamaisarah <elyamaisarah14@gmail.com>
2022-07-28 22:56:57 +0800

    update

--- insurance.sql
+++ insurance.sql
@@ -1,17 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.6.4-MariaDB-log - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             10.2.0.5599
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
 -- Dumping database structure for insurance
 CREATE DATABASE IF NOT EXISTS `insurance` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `insurance`;
@@ -75,7 +61,3 @@ CREATE TABLE IF NOT EXISTS `users` (
 REPLACE INTO `users` (`id`, `name`, `email`, `password`, `status`) VALUES
 	(1, 'Admin', 'admin@gmail.com', '011c945f30ce2cbafc452f39840f025693339c42', 0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
